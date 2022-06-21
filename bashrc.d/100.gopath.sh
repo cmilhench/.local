fd971514c9d3823b95e47c4dc90c8c6e19c8a8de
cmilhench <cmilhench@gmail.com>
2022-06-21 09:29:25 +0100

    update go path

--- bashrc.d/100.gopath.sh
+++ bashrc.d/100.gopath.sh
@@ -1,5 +1,5 @@
-if [[ -d "$HOME/Code/cm/golang" ]]; then
-  export GOPATH=$HOME/Code/cm/golang && PATH=$GOPATH/bin:$PATH
+if [[ -d "/usr/local/go" ]]; then
+  export GOPATH=/usr/local/go && PATH=$GOPATH/bin:$PATH
   export GOPRIVATE='github.com/cm-dev'
 fi
 
